8f1e241c60d49761c0884d15d71a039fd5ba056f
Anthony Li <llja0112@gmail.com>
2015-05-19 17:08:03 +0800

    Clean up spec files and directories

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,18 +2,24 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
+  describe 'GET index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET about" do
-    it "returns http success" do
+  describe 'GET about' do
+    it 'returns http success' do
       get :about
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'GET contact' do
+    it 'returns http contact' do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
